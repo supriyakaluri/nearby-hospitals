cf0334f9996aac606b54bc8d4d8d729385f5c895
supriyakaluri <supriyakaluri4091@gmail.com>
2020-11-03 10:16:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hospitalsnearby/FragmentActivity.java
@@ -0,0 +1,8 @@
+package com.example.hospitalsnearby;
+
+import android.os.Bundle;
+
+public class FragmentActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+}
---
+++ app/src/main/java/com/example/hospitalsnearby/OnMApReadyCallback.java
@@ -0,0 +1,11 @@
+package com.example.hospitalsnearby;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class OnMApReadyCallback implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
